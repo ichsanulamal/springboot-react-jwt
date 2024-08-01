ee6074ed9cc6ad2dee388c49881c8a6f1173ce6a
ichsanulamal <muhammad.ichsanul19@gmail.com>
2024-08-01 08:31:28 +0700

    update repo

--- spring-boot-spring-security-jwt-authentication-master/src/main/java/com/bezkoder/springjwt/repository/CandidateRepository.java
+++ spring-boot-spring-security-jwt-authentication-master/src/main/java/com/bezkoder/springjwt/repository/CandidateRepository.java
@@ -1,10 +1,15 @@
 package com.bezkoder.springjwt.repository;
 
 import com.bezkoder.springjwt.models.Candidate;
+import com.bezkoder.springjwt.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CandidateRepository extends JpaRepository<Candidate, Long> {
-    List<Candidate> findByUserId(Long userId);
+    Optional<Candidate> findById(Long id);
+    List<Candidate> findByUser(User user);
 }
--- spring-boot-spring-security-jwt-authentication-master/src/main/java/com/bezkoder/springjwt/repository/EducationRepository.java
+++ spring-boot-spring-security-jwt-authentication-master/src/main/java/com/bezkoder/springjwt/repository/EducationRepository.java
@@ -4,6 +4,8 @@ import com.bezkoder.springjwt.models.Education;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EducationRepository extends JpaRepository<Education, Long> {
     List<Education> findByCandidateId(Long candidateId);
